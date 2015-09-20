7da196218bac2feac76d0c35dc1fb025bfdc4667
ricardo.aguirre.99@gmail.com <ricardo.aguirre.99@gmail.com>
2015-09-20 12:58:18 -0300

    agrego una mas

--- libreriaRiganijomi/interprete.c
+++ libreriaRiganijomi/interprete.c
@@ -1,5 +1,17 @@
 #include <commons/string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/stat.h>
+#include <commons/collections/dictionary.h>
+#include <commons/config.h>
+#include <unistd.h>
+#include <commons/log.h>
 #include "interprete.h"
+
 
 int esElComando(char * linea, char * comando) {
 	string_to_lower(linea);
@@ -8,10 +20,10 @@ int esElComando(char * linea, char * comando) {
 	}
 	return 0;
 }
-/*
+
 char* devolverParteUsable(char * linea, int desde) {
 	char * cosaUsable;
 	cosaUsable = string_substring_from(linea, desde);
 	return cosaUsable;
 }
-*/
+
