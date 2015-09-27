184cb62dc818bff8d033a974c30b75d9bd3c92a1
ricardo.aguirre.99@gmail.com <ricardo.aguirre.99@gmail.com>
2015-09-27 10:43:16 -0300

    agrego funciones en cpu

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -1,3 +1,4 @@
+#include "cpu.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,11 +6,11 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
-#include <cspecs/cspec.h>
-
 #include <sys/stat.h>
 #include <commons/collections/dictionary.h>
+#include <commons/collections/list.h>
 #include <commons/config.h>
+#include <commons/string.h>
 
 #include <unistd.h>
 #include <commons/log.h>
@@ -22,145 +23,27 @@ char * ipADM;
 char * puertoADM;
 char * cantidadHilos;
 int a;
+char * retardo;
 
-int main(void) {
-	//Variables
-	int i = 0;
-	char archivo[1024];
-    char * valor;
-	int socketPlanificador;
-    int socketADM;
+int socketADM;
+int socketPlanificador;
 
-	//Prepara el archivo log al cual va a escribir
-	t_log* archivoLog = log_create("cpu.log", "CPU", false, 2);
 
-	//Levanta su configuracion y se prepara para conectarse al Planificador y al ADM
-	cargarCfgs();
-
-	for (i = 0; i < a; i++) {
-		printf("%d", i);
+int esElComando(char * package, char * comando) {
+	string_to_lower(package);
+	puts("Llego"); //BORRAR LINEA
+	if (string_starts_with(package, comando)) {
+		puts("Entro al IF"); //BORRAR LINEA
+		return 1;
 	}
-
-
-	//Se conecta al Planificador
-	socketPlanificador = socketCrearCliente(puertoPlanificador, ipPlanificador);
-	socketADM = socketCrearCliente(puertoADM, ipADM);
-	if (socketPlanificador == -1 || socketADM == -1) {
-		log_info(archivoLog, "Alguno de los dos no se pudo conectar");
-	}
-	sleep(10);
-	puts("Se creo el cliente");//TODO Borrar luego!!
-
-	//Recibe un "proceso" para ejecutar
-	socketRecibirMensaje(socketPlanificador, archivo);
-	puts("despues del receive");//TODO Borrar luego!!
-	printf("%d",archivo);
-	puts(archivo);//TODO Borrar luego!!
-	puts("voy a leer el archivo");//TODO Borrar luego!!
-    //Abre el archivo recibido por el Planificador
-	FILE *file = fopen ( archivo, "r" );
-	if ( file != NULL )  {
-		char line [ 128 ]; //Largo a definir de la linea
-		while (fgets ( line, sizeof line, file ) != NULL ) {
-		//Lee linea y ejecuta
-        if (esElComando(line, "leer")) {
-           	valor = devolverParteUsable(line, 5);
-           	instruccionLeerPagina (valor);
-
-   		}
-	   	if (esElComando(line, "finalizar")) {
-	   		instruccionFinalizarProceso();
-	   	}
-	   	if (esElComando(line, "entrada-salida")) {
-	   		valor= devolverParteUsable(line, 15);
-   	    	instruccionEntradaSalida (valor);
-	   	}
-	   	if (esElComando(line, "iniciar")) {
-	  		valor = devolverParteUsable(line, 8);
-    		instruccionIniciarProceso (valor);
-
-	  	}
-	   	if (esElComando(line, "escribir")) {
-	   		char * resultado;
-	   		resultado = string_substring(line, 9, 1);
-	   		valor = devolverParteUsable(line, 11);
-	   		instruccionEscribirPagina (resultado,valor);
-
-	   	}
-
-		//Logueo la instruccion
-		log_info(archivoLog,"envie el archivo a nico");
-		log_info(archivoLog,line);
-		puts("ya envie");//TODO Borrar luego!!
-		}
-		//Le envio mensaje al planificador cuando termino de ejecutar el proceso
-		socketEnviarMensaje(socketPlanificador, "Termine el proceso");
-		fclose ( file );
-		}else {
-		   log_error(archivoLog,"error leyendo el archivo");
-		   perror ( archivo ); /* why didn't the file open? */
-		}
 	return 0;
+	puts("No entro al IF"); //BORRAR LINEA
 }
 
-/*
-char * obtenerDirectorio(char * nombreArchivo) {
-	char directorioActual[1024];
+
+void cargarCfgs() {
 	getcwd(directorioActual, sizeof(directorioActual));
-	strcat(directorioActual, nombreArchivo);
-	return directorioActual;
-
-}*/
-
-void instruccionIniciarProceso (int * paginas) {
-	/*char * directorioActual;
-	char * puerto;
-	directorioActual = obtenerDirectorio("/src/config.cfg");
-	puts(directorioActual);
-	puerto = configObtenerPuertoMemoria(directorioActual);
-	puts(puerto);*/
-}
-
-void instruccionLeerPagina (char * resultado) {
-	/*char * directorioActual;
-	char * puerto;
-	directorioActual = obtenerDirectorio("/src/config.cfg");
-	puts(directorioActual);
-	puerto = configObtenerPuertoMemoria(directorioActual);
-	puts(puerto);*/
-}
-
-void instruccionEscribirPagina (int * pagina, char * texto) {
-	/*char * directorioActual;
-	char * puerto;
-	directorioActual = obtenerDirectorio("/src/config.cfg");
-	puts(directorioActual);
-	puerto = configObtenerPuertoMemoria(directorioActual);
-	puts(puerto);*/
-}
-
-void instruccionEntradaSalida (int * tiempo) {
-	/*char * directorioActual;
-	char * puerto;
-	directorioActual = obtenerDirectorio("/src/config.cfg");
-	puts(directorioActual);
-	puerto = configObtenerPuertoMemoria(directorioActual);
-	puts(puerto);*/
-}
-
-void instruccionFinalizarProceso() {
-	/*char * directorioActual;
-	char * puerto;
-	directorioActual = obtenerDirectorio("/src/config.cfg");
-	puts(directorioActual);
-	//puerto = configObtenerPuertoMemoria(directorioActual);
-	strcpy(puerto,"6500");
-	puts(puerto);*/
-}
-
-void cargarCfgs(){
-	getcwd(directorioActual, sizeof(directorioActual));
-	strcat(directorioActual, "/src/config.cfg\0");
+	strcat(directorioActual, "/src/config.cfg");
 
 	ipPlanificador = configObtenerIpPlanificador(directorioActual);
 	puts(ipPlanificador);//TODO Borrar luego!!
@@ -177,4 +60,179 @@ void cargarCfgs(){
 
 	puertoADM = configObtenerPuertoADM(directorioActual);
 	puts(puertoADM);//TODO Borrar luego!!
+
+	retardo = configObtenerRetardo(directorioActual);
+	puts(retardo);//TODO Borrar luego!!
 }
+
+int main(void) {
+	//Variables
+	int i = 0;
+	char archivo[1024];
+    char * valor;
+
+    t_pcb* pcb = malloc(sizeof(t_pcb));
+
+	//Prepara el archivo log al cual va a escribir
+	t_log* archivoLog = log_create("cpu.log", "CPU", false, 2);
+
+	//Levanta su configuracion y se prepara para conectarse al Planificador y al ADM
+	cargarCfgs();
+
+	for (i = 0; i < a; i++) {
+		printf("%d", i);
+	}
+
+
+	//Se conecta al Planificador
+	socketPlanificador = socketCrearCliente(puertoPlanificador, ipPlanificador);
+		//socketADM = socketCrearCliente(puertoADM, ipADM);
+	if (socketPlanificador == -1 || socketADM == -1) {
+		log_info(archivoLog, "Alguno de los dos no se pudo conectar");
+	}
+
+	puts("Se creo el cliente");//TODO Borrar luego!!
+    printf("%s",ipPlanificador);
+	//Recibe un "proceso" para ejecutar
+	//socketRecibirMensaje(socketPlanificador, archivo,1024);
+	puts("despues del receive");//TODO Borrar luego!!
+	printf("%s",archivo);
+	puts(archivo);//TODO Borrar luego!!
+	puts("voy a leer el archivo");//TODO Borrar luego!!
+	char archivo2 [1024];
+	strcpy(archivo2, "/home/utnso/rigonijami/tp-2015-2c-riganijomi/planificador/test/programa1.cod");
+
+	size_t len;
+
+    //Abre el archivo recibido por el Planificador
+	FILE * file = fopen ( archivo2, "r" );
+	puts("pasoooo");
+	if ( file != NULL )  {
+
+		char *line ; //Largo a definir de la linea
+		//while (fgets ( line, sizeof line, file ) != NULL ) {
+
+		while(line = fgetln(file, &len)){
+		//while (fgets ( line, sizeof(line),stdin ) ) {
+			//Lee linea y ejecuta
+			if (esElComando(line, "finalizar")) {
+				puts("entro al if finalizar");
+				instruccionFinalizarProceso();
+			}
+			if (esElComando(line, "leer")) {
+				puts("entro al if leer");
+				valor = devolverParteUsable(line, 5);
+				instruccionLeerPagina (valor);
+
+			}
+			if (esElComando(line, "entrada-salida")) {
+				puts("entro al if entrada-salida");
+				valor= devolverParteUsable(line, 15);
+				instruccionEntradaSalida (valor);
+			}
+			if (esElComando(line, "iniciar")) {
+				puts("entro al if iniciar");
+				valor = devolverParteUsable(line, 8);
+				instruccionIniciarProceso (valor);
+
+			}
+			if (esElComando(line, "escribir")) {
+				puts("entro al if escribir");
+				char * resultado;
+				resultado = string_substring(line, 9, 1);
+				valor = devolverParteUsable(line, 11);
+				instruccionEscribirPagina (resultado,valor);
+
+			}
+			sleep(retardo);
+			/*el log por ahora lo hace el planificador
+			//Logueo la instruccion
+			log_info(archivoLog,"envie el archivo a nico");
+			log_info(archivoLog,line);
+			puts("ya envie");//TODO Borrar luego!!
+			}
+			//Le envio mensaje al planificador cuando termino de ejecutar el proceso
+			socketEnviarMensaje(socketPlanificador, "Termine el proceso");
+
+			fclose ( file );
+			}else {
+			   log_error(archivoLog,"error leyendo el archivo");
+			   perror ( archivo ); /* why didn't the file open? */
+
+		}
+	}else{
+		puts("ES NULL");
+	}
+	return 0;
+}
+
+/*
+char * obtenerDirectorio(char * nombreArchivo) {
+	char directorioActual[1024];
+	getcwd(directorioActual, sizeof(directorioActual));
+	strcat(directorioActual, nombreArchivo);
+	return directorioActual;
+
+}*/
+
+void instruccionIniciarProceso (char * paginas) {
+	/*
+	char* respuesta;
+	socketEnviarMensaje(socketADM,paginas);
+	socketRecibirMensaje(socketADM, respuesta);
+	char * texto1;
+	char * texto2;
+	char * texto3;
+	socketEnviarMensaje(socketPlanificador,respuesta);
+	texto1 = strcat("mProc X - ",respuesta);
+	printf("%s",texto3);
+	*/
+	puts("inicia");
+	socketEnviarMensaje(socketPlanificador,paginas,1024);
+
+}
+
+void instruccionLeerPagina (char * resultado) {
+    char * contenido;
+    char * texto1;
+    char * texto2;
+    char * texto3;
+   	socketEnviarMensaje(socketADM,resultado);
+   	socketRecibirMensaje(socketADM, contenido);
+   	texto1 = strcat("mProc X - Pagina:",resultado);
+   	texto2 = strcat(texto1,"leida:");
+	texto3 = strcat(texto2,contenido);
+	printf("%s",texto3);
+
+}
+
+void instruccionEscribirPagina (int * pagina, char * texto) {
+	/*char * directorioActual;
+	char * puerto;
+	directorioActual = obtenerDirectorio("/src/config.cfg");
+	puts(directorioActual);
+	puerto = configObtenerPuertoMemoria(directorioActual);
+	puts(puerto);*/
+}
+
+void instruccionEntradaSalida (char * tiempo) {
+}
+
+void instruccionFinalizarProceso() {
+	/*
+	socketEnviarMensaje(socketADM,"finalizar");
+	printf("%s","mProc X finalizado");
+    */
+	puts("finalizar");
+	socketEnviarMensaje(socketPlanificador,"finalizar",1024);
+}
+
+
+void sem_sockets() {
+
+}
+void sem_mem() {
+
+}
+
+
--- cpu/src/cpu.h
+++ cpu/src/cpu.h
@@ -1,17 +1,23 @@
 #ifndef CPU_H_
 #define CPU_H_
 
+typedef struct PCB {
+	int processID;
+	//int estado;
+	//int programCounter;
+	char contextoEjecucion[100];
+} __attribute__ ((packed)) t_pcb;
 
 //char * obtenerDirectorio(char * nombreArchivo);
 
 void ejecutarInstrucciones();
 
-void instruccionIniciarProceso (int * paginas);
+void instruccionIniciarProceso (char * paginas);
 void instruccionLeerPagina (char * resultado);
 void instruccionEscribirPagina (int * pagina, char * texto);
-void instruccionEntradaSalida (int * tiempo);
+void instruccionEntradaSalida (char * tiempo);
 void instruccionFinalizarProceso();
-
+/*
 void cargarCfgs();
-
+*/
 #endif /* CPU_H_ */
